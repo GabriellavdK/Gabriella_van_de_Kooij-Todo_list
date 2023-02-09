8b6e11ad0245038c9608493feecd2c0c9e24beec
GabriellavdK <81296778+GabriellavdK@users.noreply.github.com>
2023-02-09 20:28:48 +0100

    latest version

--- api-client.js
+++ api-client.js
@@ -2,42 +2,50 @@ const apiUrl = "http://localhost:3000";
 
 // Get taskdata from te API server
 export async function getData() {
-    try {
-        const data = await fetch(apiUrl, { 
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        console.log("Fetched the getData from the api URL");
-        return data;
-    } catch (err) {
-        return console.log("Error: ", err);
-    }
+  try {
+    const data = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Fetched the getData from the api URL");
+    return data;
+  } catch (err) {
+    return console.log("Error: ", err);
+  }
 }
 
 // Add a new task
 export async function postData(input) {
-    try {
-        const data = await fetch(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify(input),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        console.log("Fetched the postData from the api URL");
-        return data;
-    } catch (err) {
-        return console.log("Error: ", err);
-    }
+  try {
+    const data = await fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(input),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Adjusted the postData from the api URL");
+    return data;
+  } catch (err) {
+    return console.log("Error: ", err);
+  }
 }
 
 // Delete a task
 export async function deleteData(input) {
-    try {
-
-    } catch (err) {
-        return console.log("Error: ", err);
-    }
+  try {
+    const data = await fetch("http://localhost:3000/" + input._id, {
+      method: "DELETE",
+      body: JSON.stringify(input),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Deleted the data from the api URL");
+    return data;
+  } catch (err) {
+    return console.log("Error: ", err);
+  }
 }
--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-import { getData, postData } from "./api-client.js";
+import { deleteData, getData, postData } from "./api-client.js";
 
-const todoItemlist = document.getElementsByTagName('ul')[0];
-const buttonNewTask = document.getElementsByTagName('button')[0];
+const todoItemlist = document.getElementsByTagName("ul")[0];
+const buttonNewTask = document.getElementsByTagName("button")[0];
 
 // Data ophalen van de API server
 async function receiveData(type, input) {
-    try {
-        let data = await type(input);
-        console.log("Received data from api-client.js file.");
-        data = await data.json();
-        console.log("Converted the data to JSON.");
-        return data;
-    } catch (err) {
-        return console.log(`Error: ${err}`);
-    }
+  try {
+    let data = await type(input);
+    console.log("Received data from api-client.js file.");
+    data = await data.json();
+    console.log("Converted the data to JSON.");
+    return data;
+  } catch (err) {
+    return console.log(`Error: ${err}`);
+  }
 }
 
-// Informatie uit bestaande todo list ophalen
-async function dataTodoList() {
-    const data = await receiveData(getData);
-    const descriptionTodo = data.map(i => i.description);
-    const statusTodo = data.map(i => i.done);
-    console.log(data.find(i => i.description == "jhvhg"));
-    console.log(descriptionTodo, statusTodo);
-}
-dataTodoList();
-
-// Nieuwe Todo toevoegen 
+// Nieuwe To do toevoegen
 const addNewTasktoList = (newTodo, input) => {
-    receiveData(postData, newTodo);
-    const newTask = document.createElement('li');
-    const newInput = document.createElement('input');
-    const newLabel = document.createElement('label');
-    const newImg = document.createElement('img');
-    newInput.type = 'checkbox';
-    newLabel.innerHTML = `${input}`;
-    newImg.src = 'images/trash-can.png';
-    todoItemlist.appendChild(newTask);
-    newTask.appendChild(newInput);
-    newTask.appendChild(newLabel);
-    newTask.appendChild(newImg);
-    const clickDeleteButton = document.querySelectorAll('img')[0]; // Vanaf hier de code voor de klik op de delete button
-    clickDeleteButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        
-    })
-} 
+  receiveData(postData, newTodo);
+  const newTask = document.createElement("li");
+  const newInput = document.createElement("input");
+  const newLabel = document.createElement("label");
+  const newImg = document.createElement("img");
+  newInput.type = "checkbox";
+  newLabel.innerHTML = `${input}`;
+  newImg.src = "images/trash-can.png";
+  todoItemlist.appendChild(newTask);
+  newTask.appendChild(newInput);
+  newTask.appendChild(newLabel);
+  const deleteButton = newTask.appendChild(newImg);
+  // Vanaf hier de code voor de klik op de delete button
+  deleteButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const labelTextCurrentId =
+      event.target.parentElement.getElementsByTagName("label")[0].textContent;
+    deleteTodoFromApi(labelTextCurrentId);
+  });
+};
 
-// Functionaliteit om een todo te verwijderen uit de lijst
-const deleteTodo = (data) => {
-    console.log(data);
+// Functionaliteit om een To do te verwijderen uit de lijst
+async function deleteTodoFromApi(labelTextCurrentId) {
+  const data = await receiveData(getData);
+  const detailsDeletedItem = data.find(
+    (i) => i.description == labelTextCurrentId
+  );
+  await receiveData(deleteData, detailsDeletedItem);
+  // Vanaf hier de code om de To do visueel uit de html/lijst te verwijderen
+  const getLabels = Array.from(document.getElementsByTagName("label"));
+  getLabels
+    .find((i) => i.textContent == labelTextCurrentId)
+    .parentElement.remove();
+  return;
 }
-
 
 // Functionaliteit van de knop "Add"
-buttonNewTask.addEventListener('click', (event) => {
-    event.preventDefault();
-    let valueNewTodo = document.getElementsByTagName('input')[0];
-    let input = valueNewTodo.value;
-    let newTodo = { description: input, done: false };
-    if (input == '') {
-        alert('Het veld is nog leeg!');
-    } else {
-        addNewTasktoList(newTodo, input);
-        valueNewTodo.value = '';
-    }
-})
+buttonNewTask.addEventListener("click", (event) => {
+  event.preventDefault();
+  let valueNewTodo = document.getElementsByTagName("input")[0];
+  let input = valueNewTodo.value;
+  let newTodo = { description: input, done: false };
+  if (input == "") {
+    alert("Het veld is nog leeg!");
+  } else {
+    addNewTasktoList(newTodo, input);
+    valueNewTodo.value = "";
+  }
+});
